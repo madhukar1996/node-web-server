77ad5116802693f9f5b5d748adcd0dfa5962bb6e
Madhukar <madhukar0011@gmail.com>
2018-12-06 12:26:45 +0530

    trying to upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port=process.env.PORT|| 3000
 var app=express();
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine','hbs');
-
+//
 app.use((req,res,next)=>{
 	var now=new Date().toString();
 	var log=`${now} : ${req.method} ${req.url}`
